f7791ce045b66b170c4a8c15692b5b331d4004b4
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2023-08-22 16:55:28 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -11,28 +11,29 @@ class Solution
     {
         // code here
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0, 0});
-        vector<int> vis(V, 0);
-
-		int sum = 0;
-		while (!pq.empty()) {
-			auto it = pq.top();
-			pq.pop();
-			int node = it.second;
-			int wt = it.first;
-
-			if (vis[node] == 1) continue;
-			// add it to the mst
-			vis[node] = 1;
-			sum += wt;
-			for (auto it : adj[node]) {
-				int adjNode = it[0];
-				int edW = it[1];
-				if (!vis[adjNode]) {
-					pq.push({edW, adjNode});
-				}
-			}
-		}
+        vector<int>vis(V,0);
+        pq.push({0,0});
+        int sum=0;
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            pq.pop();
+            int node=it.second;
+            int wt=it.first;
+            if(vis[node]==1)
+                continue;
+                
+            vis[node]=1;
+            sum+=wt;
+            for(auto it:adj[node]){
+                int adjNode=it[0];
+                int edw=it[1];
+                if(!vis[adjNode])
+                {
+                    pq.push({edw,adjNode});
+                }
+            }
+        }
         return sum;
     }
 };
