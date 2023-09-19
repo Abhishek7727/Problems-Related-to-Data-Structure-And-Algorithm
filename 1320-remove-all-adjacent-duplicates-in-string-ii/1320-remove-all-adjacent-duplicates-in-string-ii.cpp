f93fc67f3b4a05f70be764c6c2b0c93b463c634d
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2023-09-19 11:29:24 +0530

    Time: 28 ms (43.41%), Space: 11 MB (32.86%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-   int n = s.size();
-        if(n<k) return s;
-        
-        stack<pair<char,int>> stk;
-        for(int i=0; i<n; ++i){
-            if(stk.empty() || stk.top().first != s[i]) stk.push({s[i],1});
-            else{
-                auto prev = stk.top();
-                stk.pop();
-                stk.push({s[i], prev.second+1});
-            }
-            if(stk.top().second==k) stk.pop();
-        }
-        
-        string ans = "";
-        while(!stk.empty()){
-            auto cur = stk.top();
-            stk.pop();
-            while(cur.second--){
-                ans.push_back(cur.first);
-            }
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+     int n=s.size();
+     if(n<k)
+     return s;
+     stack<pair<char,int>>st;
+     for(int i=0;i<n;i++)
+     {
+         if(st.empty()||st.top().first!=s[i])
+         st.push({s[i],1});
+         else 
+         {
+             auto top=st.top();
+             st.pop();
+             st.push({s[i],top.second+1});
+         }
+         if(st.top().second==k)
+         st.pop();
+         
+     }
+     string ans="";
+     while(!st.empty())
+     {
+         auto curr=st.top();
+         st.pop();
+         while(curr.second--)
+         {
+             ans.push_back(curr.first);
+         }
+     }
+     reverse(ans.begin(),ans.end());
+     return ans;
         
     }
 };
