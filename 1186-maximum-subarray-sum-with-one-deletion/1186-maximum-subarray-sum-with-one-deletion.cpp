5ee3faa60a9ee0fd58e13ef03c70caf16dd49295
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-01-13 23:47:08 +0530

    Time: 0 ms (100%), Space: 27.3 MB (56.37%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -6,16 +6,18 @@ public:
         int res=nums[0];
         for(int i=1;i<nums.size();i++)
         {
-            // no power use yet
+            // no element remove  yet
             int v1=nums[i];
             int v2=nopower+nums[i];
-
+            //one element remove
             int v3=power+nums[i];
 
             int v4 =nopower;
 
             int maxsum=max(max(v1,v2),max(v3,v4));
+
             res=max(res,maxsum);
+            
             nopower=max(v1,v2);
             power= max(v3,v4);
 
