dea942a66f035731b08e25b8e4d5bc5ebf6d9f84
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-01-18 20:33:13 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,55 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int ll=0;
-        int hh=nums.size()-1;
-        while(ll<=hh)
+    int solve(vector<int>& nums)
+    {
+        int res=0;
+        int n=nums.size();
+
+        int low=0,high=n-1;
+        while(low<=high)
         {
-            int mid=(ll+hh)>>1;
-            if(nums[mid]==target)
-            return mid;
-            if(nums[mid]>=nums[ll])
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[n-1])
             {
-                if(nums[ll]<=target && target<=nums[mid])
-                hh=mid-1;
-                else
-                ll=mid+1;
+                low=mid+1;
             }
             else
             {
-                if(nums[mid]<=target && target<=nums[hh])
-                ll=mid+1;
-                
-                else 
-                hh=mid-1;
-                
+                res=mid;
+                high=mid-1;
             }
         }
-        return -1;
+        return res;
+    }
+    int binarySearch(vector<int>& nums,int k,int low,int high)
+    {
+         int res=-1;
+         int start=low,end=high;
+         while(start<=end)
+         {
+            int mid=(start+end)/2;
+            if(nums[mid]==k)
+            return mid;
+            else if(nums[mid]>k)
+            {
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+         }
+         return  res;
+
+    }
+    int search(vector<int>& nums, int target) {
+        int firstElement=solve(nums);
+        int a=binarySearch(nums,target,0,firstElement-1);
+        int b=binarySearch(nums,target,firstElement,nums.size()-1);
+        if(a==-1)
+        return b;
+        else
+        return a;
+
         
     }
 };
