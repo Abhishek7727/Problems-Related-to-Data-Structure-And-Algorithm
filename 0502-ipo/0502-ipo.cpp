bb47d5a51ed3848c9c1d67885d02f4996e892645
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-02-05 00:31:24 +0530

    Time: 71 ms (76.65%), Space: 131.4 MB (67.86%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n=profits.size();
+        vector<pair<int,int>>proj;
+        for(int i=0;i<n;i++)
+        {
+            proj.push_back({capital[i],profits[i]});
+        }
+        sort(proj.begin(),proj.end());
+        priority_queue<int>maxH;
+        int idx=0;
+        while(k--)
+        {
+            while(idx<n){
+
+            if(proj[idx].first>w)
+            break;
+            maxH.push(proj[idx].second);
+            idx++;
+            }
+            
+            if(maxH.empty())
+            return w;
+            w=w+maxH.top();
+            maxH.pop();
+
+
+
+        }
+        return w;
+        
+    }
+};
