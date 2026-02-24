c69f3ab8fec5532e3428d222532384593cffcbbf
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-02-25 01:09:37 +0530

    Time: 39 ms (33.52%), Space: 61.5 MB (10.74%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-   
-
-    int change(int a, vector<int>& coins) {
-         int n=coins.size();
-        int dp[n+1][a+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=a;j++)
-            {
-                if(i==0)
-                dp[i][j]=0;
-                if(j==0)
-                dp[i][j]=1;
-    
-            }
-        }
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++)
+    int change(int amount, vector<int>& coins) {
+        vector<vector<long>>dp(coins.size(),vector<long>(amount+1,-1));
+        return solve(coins,coins.size()-1,amount,dp);
+    }
+    int solve(vector<int>&arr,int ind,int amt,vector<vector<long>>&dp)
+    {
+        if(ind==0)
         {
-            for(int j=1;j<=a;j++)
-            {
-                if(coins[i-1]<=j)
-                dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
-                else if(coins[i-1]>j)
-                dp[i][j]=dp[i-1][j];
-            }
+        return (amt%arr[0]==0?1:0);
         }
-        return dp[n][a];
-        
+          if (dp[ind][amt] != -1)
+        return dp[ind][amt];
+
+        if(arr[ind]<=amt)
+        {
+            return dp[ind][amt]=solve(arr,ind,amt-arr[ind],dp)+solve(arr,ind-1,amt,dp);
+        }
+        else
+        return dp[ind][amt]=solve(arr,ind-1,amt,dp);
     }
 };
