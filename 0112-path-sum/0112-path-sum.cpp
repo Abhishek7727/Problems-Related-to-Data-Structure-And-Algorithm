35f1e793756ca89398e03bfe63d5b90c4ac5d3f6
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-04-03 00:05:06 +0530

    Time: 0 ms (100%), Space: 21.6 MB (34.68%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-bool solve(TreeNode* root,int k,int sum)
+bool res=false;
+void solve(TreeNode* root,int k,int sum)
     {
         if(root==NULL)
-        return false;
-        if(root->left==NULL && root->right==NULL && sum+root->val==k)
-        {
-            return true;
-        }
+        return ;
         sum+=root->val;
-        bool b1=solve(root->left,k,sum);
-        bool b2= solve(root->right,k,sum);
-        return b1||b2;
+        if(root->left==NULL && root->right==NULL )
+        {
+            if(k==sum)
+            res= true;
+            return ;
+        }
+        
+        solve(root->left,k,sum);
+        solve(root->right,k,sum);
+        return ;
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-      
-        return solve(root,targetSum,0);
+       solve(root,targetSum,0);
+        return res;
+        
         
     }
 };
