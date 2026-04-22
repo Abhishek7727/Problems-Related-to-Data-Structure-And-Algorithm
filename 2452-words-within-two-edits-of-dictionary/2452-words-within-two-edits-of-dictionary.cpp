410197ebce0d0f69e6d57ce2552bb9e69074b93d
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-04-22 21:06:04 +0530

    Time: 3 ms (74.87%), Space: 12.5 MB (88.24%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int comp(string &q, string & d)
+    {
+        int diff=0;
+        int n=q.size();
+        for(int i=0;i<n;i++)
+        {
+            if(d[i]!=q[i])
+            diff++;
+        }
+        return diff;
+    }
+    vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
+        vector<string>ans;
+
+        for(int i=0;i<q.size();i++)
+        {
+            for(int j=0;j<d.size();j++)
+            {
+                int diff=comp(q[i],d[j]);
+                if(diff<=2){
+                ans.push_back(q[i]);
+                break;
+                }
+            }
+        }
+        return ans;
+    }
+};
