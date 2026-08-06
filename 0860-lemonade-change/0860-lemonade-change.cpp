d768dbfbe3d4cff9db68171b976acb850eb707d9
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-08-06 21:52:57 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.94%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0;
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5)
+            {
+                five++;
+            }
+            else if(bills[i]==10)
+            {
+                if(five==0)
+                return false;
+                five--;
+                ten++;
+            }
+            else
+            {
+                if(ten>0)
+                {
+                    ten--;
+                    if(five==0)
+                    return false;
+                    else
+                    five--;
+                }
+                else
+                {
+                    if(five<3)
+                    return false;
+                    five=five-3;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
