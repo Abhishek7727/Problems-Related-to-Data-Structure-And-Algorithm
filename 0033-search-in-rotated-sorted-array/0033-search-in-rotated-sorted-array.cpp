885bd528cf365d8a1f01bdaf29c87122f7de8398
ABHISHEK NAMDEV <89209573+Abhishek7727@users.noreply.github.com>
2026-08-18 23:52:13 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,54 +1,29 @@
 class Solution {
 public:
-    int findFirst(vector<int>&nums)
-    {
-        int res=0;
-        int low=0,high=nums.size()-1;
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1;
         while(low<=high)
         {
-            int mid=(low+high)/2;
-            if(nums[0]<=nums[mid])
-            low=mid+1;
-            else
-            {
-                res=mid;
-                high=mid-1;
-            }
-        }
-        return res;
-    }
-    int binarySearch(vector<int>&arr,int low,int high,int target)
-    {
-       /// int res=-1;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(arr[mid]==target)
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
             return mid;
-            else if(arr[mid]>target)
+            
+            if(nums[low]<=nums[mid])
             {
+                if(nums[low]<=target && nums[mid]>target)
                 high=mid-1;
+                else
+                low=mid+1;
             }
             else
-            low=mid+1;
+            {
+                if(nums[mid]<target && nums[high]>=target)
+                low=mid+1;
+                else
+                high=mid-1;
+            }
         }
         return -1;
-    }
-    
-    int search(vector<int>& nums, int target) {
-       int n=nums.size();
-       int firstEle=findFirst(nums);
-       if(firstEle == 0)
-{
-    return binarySearch(nums,0,n-1,target);
-}
-
-       if(target>=nums[0] && target<=nums[firstEle-1])
-       {
-        return binarySearch(nums,0,firstEle-1,target);
-       }
-       else
-       return binarySearch(nums,firstEle,n-1,target);
-        
     }
 };
